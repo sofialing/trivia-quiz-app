414f2d14089cfed3f2edb93c44bb01f5cccd1cd9
Sofia Ling <sofialing@live.se>
2020-02-28 13:16:58 +0100

    Updated router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
 import StartQuiz from './components/StartQuiz'
 import Quiz from './components/Quiz'
 import Result from './components/Result'
@@ -7,14 +7,14 @@ import Result from './components/Result'
 class App extends Component {
 	render() {
 		return (
-			<Router>
+			<HashRouter>
 				<Switch>
 					<Route exact path='/' component={StartQuiz}></Route>
 					<Route path='/quiz' component={Quiz}></Route>
 					<Route path='/result' component={Result}></Route>
 					<Redirect to='/' />
 				</Switch>
-			</Router>
+			</HashRouter>
 		)
 	}
 }
