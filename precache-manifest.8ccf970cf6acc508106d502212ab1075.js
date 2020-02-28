343635dcf373e2d91ccd652e72e0956f8ef11c3d
Sofia Ling <sofialing@live.se>
2020-02-28 11:53:17 +0100

    Updates

---
+++ precache-manifest.8ccf970cf6acc508106d502212ab1075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8230a6680f0847192d5c15b9dd2044a5",
+    "url": "/quiz-trivia-app/index.html"
+  },
+  {
+    "revision": "b0c809b8ae8ad9078344",
+    "url": "/quiz-trivia-app/static/css/main.73d3abe3.chunk.css"
+  },
+  {
+    "revision": "6f586cde100bdd7c5419",
+    "url": "/quiz-trivia-app/static/js/2.8e3c0e8c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/quiz-trivia-app/static/js/2.8e3c0e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c809b8ae8ad9078344",
+    "url": "/quiz-trivia-app/static/js/main.c182edd9.chunk.js"
+  },
+  {
+    "revision": "090cdd78ee5a214ce44f",
+    "url": "/quiz-trivia-app/static/js/runtime-main.5b25e65e.js"
+  }
+]);
